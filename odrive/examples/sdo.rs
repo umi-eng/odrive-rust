69e6dddd8420f0f2ce1a209755bfc7b7021f3f4a
Liam Kinne <liam@kinne.io>
2025-06-21 04:07:21 +1000

    fix formatting

--- odrive/examples/sdo.rs
+++ odrive/examples/sdo.rs
@@ -11,7 +11,7 @@ async fn main() -> io::Result<()> {
 
     // Read the bus voltage using an SDO read with an endpoint id of 1.
     let vbus = odrive.sdo_read(1, ValueKind::Float).await?;
-    println!("VBUS: {}", vbus);
+    println!("VBUS: {:?}", vbus);
 
     // The given endpoint id for a parameter can be found in the
     // flat_endpoints.json file provided with each ODrive firmware release.
