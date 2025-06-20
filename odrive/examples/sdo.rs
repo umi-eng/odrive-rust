fa5de6403de6c0507708c8fb61f83c0b527c3588
Liam Kinne <liam@kinne.io>
2025-06-21 02:15:02 +1000

    add sdo example

---
+++ odrive/examples/sdo.rs
@@ -0,0 +1,20 @@
+use std::io;
+
+use odrive::can::{ODrive, ValueKind};
+use socketcan::tokio::CanSocket;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    // Setup the ODrive connection
+    let socket = CanSocket::open("can0")?;
+    let odrive = ODrive::new(socket, 1);
+
+    // Read the bus voltage using an SDO read with an endpoint id of 1.
+    let vbus = odrive.sdo_read(1, ValueKind::Float).await?;
+    println!("VBUS: {}", vbus);
+
+    // The given endpoint id for a parameter can be found in the
+    // flat_endpoints.json file provided with each ODrive firmware release.
+
+    Ok(())
+}
